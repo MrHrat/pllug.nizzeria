d40fb231abdf093973dc04d4cef5259bb7852445
MrHrat <hrat1995@gmail.com.com>
2019-01-07 23:46:14 +0200

    Add Null pizza class

---
+++ Nizzeria/Class/TypesOfPizza/NullPizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nizzeria.Class.TypesOfPizza
+{
+    public class NullPizza : Pizza
+    {
+        public NullPizza() : base("Not selected pizza") { }
+
+        public override double Cost()
+        {
+            return 0;
+        }
+    }
+}
