c8fbfccc465e0bb4df867d8145937b22aa5e895b
MrHrat <hrat1995@gmail.com.com>
2019-01-07 23:43:57 +0200

    Add converter Description Pizza

---
+++ Nizzeria/Class/Converter/PizzaToDescriptionStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Nizzeria.Class.TypesOfPizza;
+
+namespace Nizzeria.Class.Converter
+{
+    public class PizzaToDescriptionStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var pizza = value as Pizza;
+            return pizza.GetDescription();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
